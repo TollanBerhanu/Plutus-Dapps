300735adb7560a0fba7a8e4a4d92778c2973a403
TollanBerhanu <tollanberhanu@gmail.com>
2023-05-12 05:54:56 +0000

    Modified Off chain code

--- simple-mint-sc/Off-Chain/src/Base/layout.tsx
+++ simple-mint-sc/Off-Chain/src/Base/layout.tsx
@@ -1,29 +1,47 @@
-// import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState, useEffect, useCallback } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MintOrBurn from './lucid';
+import { offChain } from './lucid';
 
 
 const Layout = () => {
   
-  const [amount, setAmount] : any = useState(0)
+  const [amount, setAmount]: any = useState(0)
+  const [signedTx, setSignedTx]: any = useState('')
+
+  useEffect(() => {
+    offChain(10n).then(s => {
+      console.log(s)
+    })
+  },[])
+
+  const getSignedTx = () => {
+    console.log('Hello')
+      // setSignedTx(s)
+  }
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Amount</Form.Label>
-        <Form.Control type="text" placeholder="Enter amount" value={amount} onChange={(event) => setAmount(event.target.value)}/>
-        <Form.Text className="text-muted">
-          {amount}
-        </Form.Text>
-      </Form.Group>
-      {/* <Button variant="primary" type="submit">
-        Submit
-      </Button> */}
-      <MintOrBurn amount={amount} />
-      
-    </Form>
+    // <Form>
+    //   <Form.Group className="mb-3" controlId="formBasicEmail">
+    //     <Form.Label>Amount</Form.Label>
+    //     <Form.Control type="text" placeholder="Enter amount"/>
+    //     <Form.Text className="text-muted">
+    //       {/* {amount} */}
+    //     </Form.Text>
+    //   </Form.Group>
+
+    //   <Button onClick={getSignedTx} variant="primary" type="submit">
+    //     Submit
+    //   </Button>
+    //   {/* <p>{signedTx}</p> */}
+    // </Form>
+    <>
+      <input type='number' value={amount} onChange={(event) => setAmount(event.target.value)} />
+      <p>{amount}</p>
+      <button>Hello</button>
+      <p> {signedTx} </p>
+    </>
   );
 }
 
