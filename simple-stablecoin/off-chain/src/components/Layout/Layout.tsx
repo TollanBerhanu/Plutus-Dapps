554d2b6aa3a22139508ca9849d14cdcbc57dbd12
TollanBerhanu <tollanberhanu@gmail.com>
2023-07-04 13:40:09 +0000

    feat: add layout component

---
+++ simple-stablecoin/off-chain/src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Nav } from '../Nav/Nav'
+
+export const Layout = () => (
+    <div className="Layout">
+        <Nav />
+        <main className="content">
+             Main
+        </main>
+    </div>
+)
